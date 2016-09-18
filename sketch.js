bc749f61e086b4c6bdceb4dbe6ae73cd560010e3
Sofia Garcia <sofisagarcia@gmail.com>
2016-09-18 17:23:23 -0400

    y axis no longer maps to iput, width and height mapped instead

--- sketch.js
+++ sketch.js
@@ -16,16 +16,20 @@ function draw() {
   var vol = mic.getLevel();
   
   var x = width/2;
-  var y = map(vol, 0, 1, height/2, height);
+  var y = height/2;
+
+  var eyeSize = map(vol, 0, 1, 30, 185);
+  var headSize = map(vol, 0, 1, 200, 350);
   var mouthSize = map(vol, 0, 1, 50, 150);
+  
   //head
   fill('#f5c833');
-  ellipse(x, y, 200, 200);
+  ellipse(x, y, headSize, headSize);
   
   //eyes
   fill(255, 255, 255);
-  ellipse(x - 50, y-25, 30, 30);
-  ellipse(x + 50, y-25, 30, 30);
+  ellipse(x - 50, y-25, eyeSize, eyeSize);
+  ellipse(x + 50, y-25, eyeSize, eyeSize);
   
   //mouth
   fill('#77ccf1');
